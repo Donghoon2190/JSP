135eb439877342c46ffdf34c6be4d26c45e6d1ee
Donghoon2190 <73936593+Donghoon2190@users.noreply.github.com>
2020-12-23 10:36:52 +0900

    Add files via upload

---
+++ example2/src/Dto/MemberInfoBean.java
@@ -0,0 +1,33 @@
+package Dto;
+
+public class MemberInfoBean {
+	private String meId;
+	private String mePw;
+	private String meName;
+	private String meEmail;
+	public String getMeId() {
+		return meId;
+	}
+	public void setMeId(String meId) {
+		this.meId = meId;
+	}
+	public String getMePw() {
+		return mePw;
+	}
+	public void setMePw(String mePw) {
+		this.mePw = mePw;
+	}
+	public String getMeName() {
+		return meName;
+	}
+	public void setMeName(String meName) {
+		this.meName = meName;
+	}
+	public String getMeEmail() {
+		return meEmail;
+	}
+	public void setMeEmail(String meEmail) {
+		this.meEmail = meEmail;
+	}
+	
+}
